d5cbdd37bb0b357049e67cd35f37ca92aafda01c
yuki <philippaltekoster@gmail.com>
2023-01-29 23:04:48 +0100

    monkai theme

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -14,6 +14,7 @@ local packer_bootstrap = ensure_packer()
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use 'ellisonleao/gruvbox.nvim'
+  use 'tanvirtin/monokai.nvim'
   use 'nvim-tree/nvim-tree.lua'
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-lualine/lualine.nvim'
